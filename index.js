b5e6ccba5fcdaa951894396f245aaedc1a218725
stanli206 <stantilin@gmail.com>
2025-03-26 19:47:51 +0530

    latest commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:5173", //
+    origin: "https://resetpasswordtask.netlify.app", //
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
